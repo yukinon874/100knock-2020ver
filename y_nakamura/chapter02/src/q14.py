356b1a8b29f2eb611671aa3ad9dabd40e92dcd4c
仲村祐希 <y_nakamura@nakamurayuukinoMacBook-Air.local>
2020-04-20 16:07:02 +0900

    名前の修正

---
+++ y_nakamura/chapter02/src/q14.py
@@ -0,0 +1,7 @@
+import sys
+
+n = int(sys.argv[1])
+with open('./data/popular-names.txt', mode = 'r') as f:
+    data_list = f.readlines()
+    for data in data_list[:n]:
+        print(data.strip())
